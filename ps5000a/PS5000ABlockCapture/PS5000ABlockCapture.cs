63697a1cac8c38ea83222d93c94ff8c59f99cdb6
ДМ. Па <qtckpuhdsa@gmail.com>
2020-05-28 17:01:29 +0300

    Доработки и переработки интерфеса 1

---
+++ ps5000a/PS5000ABlockCapture/PS5000ABlockCapture.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace PS5000A
+{
+    static class PS5000ABlockCapture
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PS5000ABlockForm(0.034, 3.4, 600));
+        }
+    }
+}
---
+++ ps5000a/PS5000ABlockCapture/PS5000ABlockForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+
+namespace PS5000A
+{
+    partial class PS5000ABlockForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        
+        //true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        //private System.Windows.Forms.Button buttonOpen;
+        //private System.Windows.Forms.TabControl tabControl;
+        //private System.Windows.Forms.TabPage tabUnit;
+        //private System.Windows.Forms.TabPage tabChannels;
+        //private System.Windows.Forms.ComboBox comboRangeA;
+        //private System.Windows.Forms.TabPage tabAbout;
+        //private System.Windows.Forms.TextBox textBoxUnitInfo;
+        //private System.Windows.Forms.ComboBox comboRangeB;
+        //private System.Windows.Forms.ComboBox comboRangeC;
+        //private System.Windows.Forms.ComboBox comboRangeD;
+        //private System.Windows.Forms.TabPage tabGetData;
+        //private System.Windows.Forms.TextBox textData;
+        //private System.Windows.Forms.Button buttonStart;
+        //private System.Windows.Forms.TextBox textMessage;
+        //private System.Windows.Forms.Label label1;
+        //private System.Windows.Forms.TextBox textBox1;
+        //private System.Windows.Forms.TextBox textBox7;
+        //private System.Windows.Forms.Label label10;
+        //private System.Windows.Forms.Label label11;
+        //private System.Windows.Forms.Label label12;
+        //private System.Windows.Forms.TextBox textBox8;
+        //private System.Windows.Forms.TextBox textBox5;
+        //private System.Windows.Forms.Label label7;
+        //private System.Windows.Forms.Label label8;
+        //private System.Windows.Forms.Label label9;
+        //private System.Windows.Forms.TextBox textBox6;
+        //private System.Windows.Forms.TextBox textBox3;
+        //private System.Windows.Forms.Label label4;
+        //private System.Windows.Forms.Label label5;
+        //private System.Windows.Forms.Label label6;
+        //private System.Windows.Forms.TextBox textBox4;
+        //private System.Windows.Forms.TextBox textBox2;
+        //private System.Windows.Forms.Label label3;
+        //private System.Windows.Forms.Label label2;
+        //private System.Windows.Forms.GroupBox groupBox1;
+        //private System.Windows.Forms.StatusStrip statusStrip1;
+        //private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        //private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        //private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox comboRangeA;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
